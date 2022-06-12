2125c320df6a9bf0170236dddba393c33d0a3676
SolomonAvraham <Selmon_a@hotmail.com>
2022-06-12 14:19:19 +0300

    update

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
 function clickSubmit() {
-  if (
-    (checkRePassword(),
-    checkPassword(),
+  if ((checkPassword(),
     checkPhone(),
     checkEmail(),
     checkLastName(),
@@ -38,32 +36,49 @@ function checkLastName() {
 }
 
 function birthDate() {
- 
-
   if (birthDateId.value <= "2004-01-01") {
-    alert("true3");
     return true;
   }
-  alert("false3");
   birthDateLabel.innerHTML += "<br> <small> * Must Be 18!* </small>";
   birthDateLabel.style = "color:red";
   return false;
-
-
 }
 
 function checkEmail() {
   regex =
     /(?:^|\s)[\w!#$%&'*+/=?^`{|}~-](\.?[\w!#$%&'*+/=?^`{|}~-]+)*@HELLO.com/;
-  if (true) {
+  if (regex.test(emailId.value)) {
     return true;
   }
-
   return false;
 }
 
-function checkPhone() {}
+function checkPhone() {
+  regex =
+    /^(?:(?:(\+?972|\(\+?972\)|\+?\(972\))(?:\s|\.|-)?([1-9]\d?))|(0[23489]{1})|(0[57]{1}[0-9]))(?:\s|\.|-)?([^0\D]{1}\d{2}(?:\s|\.|-)?\d{4})$/;
 
-function checkPassword() {}
+  if (regex.test(telId.value)) {
+    return true;
+  }
+  telLabel.innerHTML +=
+    "<br><small>*Must Enter This Format 050-1111111*</small>";
+  telLabel.style.color = "red";
+  return false;
+}
 
-function checkRePassword() {}
+function checkPassword() {
+  var strongRegex =
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,12})/;
+  if (password1Id.value == password2Id.value) {
+    if (strongRegex.test(password1Id.value == password2Id.value)) {
+      return true;
+    }
+    password1Label.innerHTML += "<br>*<small>Password is not strong enough</small>*";
+    password1Label.style.color = "red";
+    return false;
+    
+  }
+  password2Label.innerHTML += "<br>*<small>Passwords not match</small>*";
+  password2Label.style.color = "red";
+  return false;
+}
